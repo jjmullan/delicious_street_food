19db47eccc4bf3ef68e8844e93be255d13190035
최영준 <jjmullan24@gmail.com>
2025-11-04 01:29:25 +0900

    feat: Tanstack Query 전역 설정 추가

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,7 +5,14 @@ import { router } from './router/routes';
 import '@/app/styles/main.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			staleTime: 0,
+			gcTime: 1_000 * 60 * 5, // 5분 캐싱
+		},
+	},
+});
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
