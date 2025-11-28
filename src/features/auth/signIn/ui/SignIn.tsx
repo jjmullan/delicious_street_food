7e39502aed09b992e3fff841018a92b4b2e953fd
최영준 <jjmullan24@gmail.com>
2025-11-28 22:57:31 +0900

    [session] feat: 카카오 소셜로그인 기능 추가
    ISSUE #7

--- src/features/auth/signIn/ui/SignIn.tsx
+++ src/features/auth/signIn/ui/SignIn.tsx
@@ -37,6 +37,9 @@ function SignIn() {
 	const handleClickSignInWithGoogle = () => {
 		signInWithOAuth('google');
 	};
+	const handleClickSignInWithKakao = () => {
+		signInWithOAuth('kakao');
+	};
 
 	// 비밀번호 On/Off
 	const [passwordOpen, setPasswordOpen] = useState(false);
@@ -99,6 +102,7 @@ function SignIn() {
 				<button
 					type="button"
 					className="bg-[#FEE500] text-[#000000] py-5 inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive h-9 px-4 w-full cursor-pointer"
+					onClick={handleClickSignInWithKakao}
 				>
 					카카오 로그인
 				</button>
