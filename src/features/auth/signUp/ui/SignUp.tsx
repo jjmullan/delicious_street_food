cc9a06afd9a5100f630c4f96051a6b22cf8a7dcd
최영준 <jjmullan24@gmail.com>
2025-11-28 16:05:14 +0900

    [loginUI] feat: 이메일 회원가입 기능 추가
    - 입력된 이메일, 비밀번호를 바탕으로 supabase 에 회원가입
    - 예외사항 오류 메시지 제공 기능 추가
    
    ISSUE #3

---
+++ src/features/auth/signUp/ui/SignUp.tsx
@@ -0,0 +1,105 @@
+import { Activity, useState } from 'react';
+import { useNavigate } from 'react-router';
+import { toast } from 'sonner';
+import useSignUpWithEmail from '@/features/auth/signUp/hooks/useSignUpWithEmail';
+import validateEmail from '@/features/auth/signUp/util/validateEmail';
+import validatePassword from '@/features/auth/signUp/util/validatePassword';
+import { generateErrorMessage } from '@/shared/lib/error';
+import { Button } from '@/shared/ui/shadcn/button';
+import { Input } from '@/shared/ui/shadcn/input';
+
+function SignUp() {
+	const navigate = useNavigate();
+
+	// 제출 여부 상태
+	const [isSubmitted, setIsSubmitted] = useState(false);
+
+	// 회원가입 입력요소 상태
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [passwordConfirm, setPasswordConfirm] = useState('');
+
+	// 정규표현식 오류 상태
+	const isEmailValid = validateEmail(email);
+	const isPasswordValid = validatePassword(password, passwordConfirm);
+
+	// 회원가입 API 전송
+	const { mutate: signUp, isPending } = useSignUpWithEmail({
+		onSuccess: () => {
+			navigate('/', { replace: true });
+			toast.success('회원가입이 완료되었습니다🎉', {
+				position: 'top-center',
+			});
+		},
+		onError: (error) => {
+			const message = generateErrorMessage(error);
+			toast.error(message, { position: 'top-center' });
+		},
+	});
+	const handleClickSubmit = () => {
+		setIsSubmitted(true);
+
+		if (!isEmailValid.isValid || !isPasswordValid.isValid) {
+			return;
+		}
+
+		signUp({ email, password });
+	};
+
+	return (
+		<div className="flex flex-col gap-y-4">
+			<div className="flex flex-col gap-y-2">
+				<div className="flex gap-x-2">
+					<Input
+						type="email"
+						placeholder="이메일"
+						className="py-5"
+						disabled={isPending}
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+					/>
+					<Button type="button" className="py-5 cursor-pointer" disabled={isPending}>
+						중복 확인
+					</Button>
+				</div>
+				<Activity mode={isSubmitted && isEmailValid.errors.email ? 'visible' : 'hidden'}>
+					<p className="text-sm text-muted-foreground px-2">{isEmailValid.errors.email}</p>
+				</Activity>
+			</div>
+			<div className="flex flex-col gap-y-2">
+				<div className="flex flex-col gap-y-2">
+					<Input
+						type="password"
+						placeholder="비밀번호"
+						name="password"
+						className="py-5"
+						disabled={isPending}
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+					<Activity mode={isSubmitted && isPasswordValid.errors.password ? 'visible' : 'hidden'}>
+						<p className="text-sm text-muted-foreground px-2">{isPasswordValid.errors.password}</p>
+					</Activity>
+				</div>
+				<div className="flex flex-col gap-y-2">
+					<Input
+						type="password"
+						placeholder="비밀번호 재입력"
+						className="py-5"
+						disabled={isPending}
+						value={passwordConfirm}
+						onChange={(e) => setPasswordConfirm(e.target.value)}
+					/>
+					<Activity mode={isSubmitted && isPasswordValid.errors.passwordConfirm ? 'visible' : 'hidden'}>
+						<p className="text-sm text-muted-foreground px-2">{isPasswordValid.errors.passwordConfirm}</p>
+					</Activity>
+				</div>
+			</div>
+			<Button className="w-full py-5 cursor-pointer" disabled={isPending} onClick={handleClickSubmit}>
+				{isPending ? '회원가입 진행 중' : '회원가입'}
+			</Button>
+		</div>
+	);
+}
+
+export default SignUp;
--- src/pages/signUp/SignUpPage.tsx
+++ src/pages/signUp/SignUpPage.tsx
@@ -1,17 +1,10 @@
-import { Button } from '@/shared/ui/button';
-import { Input } from '@/shared/ui/input';
+import SignUp from '@/features/auth/signUp/ui/SignUp';
 
 function SignUpPage() {
 	return (
-		<div className="flex flex-col justify-center gap-y-2 w-1/2">
-			<div className="flex gap-x-2">
-				<Input type="email" placeholder="*이메일" className="py-4" />
-				<Button type="button">중복 확인</Button>
-			</div>
-			<Input type="password" placeholder="*비밀번호" className="py-4" />
-			<Input type="password" placeholder="*비밀번호 재입력" className="py-4" />
-			<Input type="text" placeholder="닉네임" />
-			<Input type="text" placeholder="한 줄 소개" />
+		<div className="flex flex-col justify-center gap-y-5 min-w-1/3 max-w-1/2 mx-auto">
+			<h2>회원가입</h2>
+			<SignUp />
 		</div>
 	);
 }
