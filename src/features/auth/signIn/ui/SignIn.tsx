e8fadec5689ac2afcf220707daf7fa595266dde3
최영준 <jjmullan24@gmail.com>
2025-11-30 14:37:44 +0900

    [kakaoMapAPI] style: 카카오 로그인 버튼 위지 조정
    ISSUE #10

--- src/features/auth/signIn/ui/SignIn.tsx
+++ src/features/auth/signIn/ui/SignIn.tsx
@@ -94,17 +94,17 @@ function SignIn() {
 				</Button>
 				<button
 					type="button"
-					className="bg-white text-black py-5 inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive h-9 px-4 w-full cursor-pointer border"
-					onClick={handleClickSignInWithGoogle}
-				>
-					구글 로그인
-				</button>
-				<button
-					type="button"
 					className="bg-[#FEE500] text-[#000000] py-5 inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive h-9 px-4 w-full cursor-pointer"
 					onClick={handleClickSignInWithKakao}
 				>
 					카카오 로그인
+				</button>
+				<button
+					type="button"
+					className="bg-white text-black py-5 inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive h-9 px-4 w-full cursor-pointer border"
+					onClick={handleClickSignInWithGoogle}
+				>
+					구글 로그인
 				</button>
 			</div>
 			<div className="flex gap-x-2 text-muted-foreground">
