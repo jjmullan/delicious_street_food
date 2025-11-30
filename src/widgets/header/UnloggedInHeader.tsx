3de10c2384461100bf1b959d614a561907b0cc81
최영준 <jjmullan24@gmail.com>
2025-11-30 23:05:22 +0900

    fix: 모바일 UI 기반 높이가 안맞는 오류 해결
    - 전체 높이에서 헤더 높이만큼 뺀 값을 계산

--- src/widgets/header/UnloggedInHeader.tsx
+++ src/widgets/header/UnloggedInHeader.tsx
@@ -6,7 +6,7 @@ function UnloggedInHeader() {
 	const navigate = useNavigate();
 
 	return (
-		<header className="flex justify-between items-center px-4 h-12 text-lg w-full">
+		<header className="flex justify-start items-center px-4 h-12 text-lg w-full max-w-[768px] md:border-x mx-auto">
 			<Activity mode={window.location.href.split('/login').pop() === '' ? 'hidden' : 'visible'}>
 				<button type="button" onClick={() => navigate(-1)}>
 					<ArrowLeft />
