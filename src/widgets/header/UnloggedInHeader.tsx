e26920641d26dc7f20d2f6ef8aaeb96a4480d95c
최영준 <jjmullan24@gmail.com>
2025-11-30 21:38:53 +0900

    [loginUI] rename: Header 명칭 변경 및 기능 추가
    - login 페이지가 아닌 경우 뒤로가기 UI 제공
    
    ISSUE #12

--- src/widgets/header/UnloggedInHeader.tsx
+++ src/widgets/header/UnloggedInHeader.tsx
@@ -1,34 +1,19 @@
+import { ArrowLeft } from 'lucide-react';
 import { Activity } from 'react';
-import { Link, useNavigate } from 'react-router';
-import { useSession } from '@/app/store/sessionStore';
+import { useNavigate } from 'react-router';
 
 function UnloggedInHeader() {
-	const session = useSession();
 	const navigate = useNavigate();
 
 	return (
 		<header className="flex justify-between items-center px-4 h-12 text-lg w-full">
-			<div className="w-[100px]">
-				<Activity mode={window.location.href.split('/').pop() === '' ? 'hidden' : 'visible'}>
-					<button type="button" onClick={() => navigate(-1)}>
-						뒤로 가기
-					</button>
-				</Activity>
-			</div>
-			<h1 className="text-center">
-				<Link to={'/'}>로고</Link>
-			</h1>
-			<div className="w-[100px] flex justify-end">
-				{session ? (
-					<button type="button">로그아웃</button>
-				) : (
-					<button type="button" onClick={() => navigate('/login')} className="cursor-pointer">
-						로그인
-					</button>
-				)}
-			</div>
+			<Activity mode={window.location.href.split('/login').pop() === '' ? 'hidden' : 'visible'}>
+				<button type="button" onClick={() => navigate(-1)}>
+					<ArrowLeft />
+				</button>
+			</Activity>
 		</header>
 	);
 }
-4;
+
 export default UnloggedInHeader;
--- src/widgets/layout/UnloggedInLayout.tsx
+++ src/widgets/layout/UnloggedInLayout.tsx
@@ -1,6 +1,6 @@
 import { Navigate, Outlet } from 'react-router';
 import { useSession } from '@/app/store/sessionStore';
-import PublicHeader from '@/widgets/header/UnloggedInHeader';
+import UnloggedInHeader from '@/widgets/header/UnloggedInHeader';
 
 /**
  * 최초 서비스 접근 시, 로그인 페이지로 라우팅
@@ -12,7 +12,7 @@ function UnloggedInLayout() {
 	return (
 		<>
 			{/* 헤더 영역 */}
-			<PublicHeader />
+			<UnloggedInHeader />
 			<Outlet />
 		</>
 	);
