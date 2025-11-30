75fdfc266efee5c4fc201eba9133661d8490a07a
최영준 <jjmullan24@gmail.com>
2025-11-30 17:34:02 +0900

    [loginUI] feat: 로그인 UI 1차
    - 로고 이미지 및 타이틀 추가
    
    ISSUE #12

--- src/features/auth/signIn/ui/SignInCommon.tsx
+++ src/features/auth/signIn/ui/SignInCommon.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router';
 import useSingInWithOAuth from '@/features/auth/signIn/hooks/mutation/useSignInWithOAuth';
+import logo from '/logo.svg';
 import emailSvg from '/logo-email.svg';
 import google from '/logo-google.svg';
 import kakao from '/logo-kakao.svg';
@@ -20,36 +21,44 @@ function SignInCommon() {
 
 	return (
 		<div className="flex flex-col gap-y-8">
-			<div className="flex flex-col gap-y-2">
-				{/* 이메일 로그인 버튼 */}
-				<button
-					type="button"
-					className="bg-black text-white py-5 inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive h-9 px-4 w-full cursor-pointer relative"
-					onClick={handleClickSignInWithPassword}
-					disabled={isPending}
-				>
-					<img src={emailSvg} alt="email login" width={'20'} className="absolute left-4" />
-					이메일 로그인
-				</button>
-				{/* 소셜 로그인 */}
-				<button
-					type="button"
-					className="bg-[#FEE500] text-[#000000] py-5 inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive h-9 px-4 w-full cursor-pointer relative"
-					onClick={handleClickSignInWithKakao}
-					disabled={isPending}
-				>
-					<img src={kakao} alt="kakao login" width={'20'} className="absolute left-4 aspect-square" />
-					카카오 로그인
-				</button>
-				<button
-					type="button"
-					className="bg-white text-black py-5 inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive h-9 px-4 w-full cursor-pointer border relative"
-					onClick={handleClickSignInWithGoogle}
-					disabled={isPending}
-				>
-					<img src={google} alt="google login" width={'20'} className="absolute left-4 aspect-square" />
-					<p>구글 로그인</p>
-				</button>
+			<div className="flex flex-col gap-y-12">
+				{/* 타이틀 */}
+				<div className="flex flex-col items-center gap-y-6">
+					<img src={logo} alt="logo" className="w-full max-w-[200px]" />
+					<h2 className="title">포장맛차</h2>
+				</div>
+				{/* 로그인 버튼 */}
+				<div className="flex flex-col gap-y-2">
+					{/* 이메일 로그인 */}
+					<button
+						type="button"
+						className="bg-black text-white py-5 inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive h-9 px-4 w-full cursor-pointer relative"
+						onClick={handleClickSignInWithPassword}
+						disabled={isPending}
+					>
+						<img src={emailSvg} alt="email login" width={'20'} className="absolute left-4" />
+						이메일 로그인
+					</button>
+					{/* 소셜 로그인 */}
+					<button
+						type="button"
+						className="bg-[#FEE500] text-[#000000] py-5 inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive h-9 px-4 w-full cursor-pointer relative"
+						onClick={handleClickSignInWithKakao}
+						disabled={isPending}
+					>
+						<img src={kakao} alt="kakao login" width={'20'} className="absolute left-4 aspect-square" />
+						카카오 로그인
+					</button>
+					<button
+						type="button"
+						className="bg-white text-black py-5 inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive h-9 px-4 w-full cursor-pointer border relative"
+						onClick={handleClickSignInWithGoogle}
+						disabled={isPending}
+					>
+						<img src={google} alt="google login" width={'20'} className="absolute left-4 aspect-square" />
+						<p>구글 로그인</p>
+					</button>
+				</div>
 			</div>
 		</div>
 	);
