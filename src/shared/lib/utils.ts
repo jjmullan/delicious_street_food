b8131d198abe0e485d9e677ef8367ac29bd69720
최영준 <jjmullan24@gmail.com>
2025-12-02 01:31:01 +0900

    [emailCheck] feat: 유저 랜덤 닉네임 생성 유틸 함수 추가
    - 50 * 20 * 9999 = 9,999,000 가지의 닉네임 생성 가능
    
    ISSUE #16

--- src/shared/lib/utils.ts
+++ src/shared/lib/utils.ts
@@ -1,6 +1,14 @@
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
+import { expressions, foods } from '@/shared/lib/nickname';
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
+}
+
+export function getRandomUserNickname() {
+	const expression = expressions[Math.floor(Math.random() * expressions.length)];
+	const food = foods[Math.floor(Math.random() * foods.length)];
+	const number = Math.floor(Math.random() * 9_999);
+	return `${expression}${food}${number}`;
 }
