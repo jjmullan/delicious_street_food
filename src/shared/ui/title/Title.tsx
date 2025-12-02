9aed4ede44ec528a15d884807a4a50087eb19797
최영준 <jjmullan24@gmail.com>
2025-12-02 20:59:18 +0900

    [mapCategoryButton] fix: 타이틀 슬로건 텍스트 수정
    ISSUE #20

--- src/shared/ui/title/Title.tsx
+++ src/shared/ui/title/Title.tsx
@@ -2,7 +2,7 @@ function Title() {
 	return (
 		<div className="flex flex-col justify-center items-center gap-y-1 text-justify">
 			{/* <img src={logo} alt="logo" className="w-[120px]" /> */}
-			<p className="text-lg">길거리 음식이 그리울 땐</p>
+			<p className="text-lg">길거리 음식이 생각날 때</p>
 			<h2 className="title text-5xl">포장맛차</h2>
 		</div>
 	);
