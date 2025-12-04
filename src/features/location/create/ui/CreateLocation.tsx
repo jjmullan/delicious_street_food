e19b608901d9e929254e31a900daf6969c6fa9c6
최영준 <jjmullan24@gmail.com>
2025-12-04 19:37:50 +0900

    [requestLocationData] style: 클릭 시 선택창 나오도록 개선
    ISSUE #22

--- src/features/location/create/ui/CreateLocation.tsx
+++ src/features/location/create/ui/CreateLocation.tsx
@@ -1,18 +1,25 @@
-import LocationFinder from '@/features/location/fetch/ui/LocationFinder';
+import { CircleXIcon } from 'lucide-react';
+import { items } from '@/features/product/item/libs/item';
 
 function CreateLocation() {
 	return (
-		<div className="relative flex flex-col items-center gap-y-1">
-			<LocationFinder bgColorCode="1" />
-
-			{/* <img src={} alt="eggbun" className="h-5 w-auto object-contain aspect-square" /> */}
-			{/* <div className="flex flex-col gap-y-1 glass rounded-full w-full z-2">
-				{items.map((item) => (
-					<button key={item.image_url} type="button" className="flex items-center justify-center gap-x-2 w-6 h-6">
-						<img src={item.image_url} alt="eggbun" className="h-5 w-auto object-contain aspect-square" />
+		<div className="relative">
+			{/* <LocationFinder bgColorCode="1" /> */}
+			<div className="absolute translate-[-16px] glass rounded-full w-fit z-2 p-0.5">
+				<div className="flex flex-col items-center gap-y-0.5">
+					<button type="button" className="w-6 h-6 flex items-center justify-center border-b">
+						<CircleXIcon className="w-4 h-4" />
 					</button>
-				))}
-			</div> */}
+					<div className="flex flex-col items-center gap-y-1">
+						{items.map((item) => (
+							<button key={item.image_url} type="button" className="flex items-center justify-center gap-x-2 w-6 h-6">
+								<img src={item.image_url} alt="eggbun" className="h-5 w-auto object-contain aspect-square" />
+							</button>
+						))}
+					</div>
+				</div>
+			</div>
+
 			{/* <div className="absolute top-8">
 				<Activity mode={isModalOpen ? 'visible' : 'hidden'}>
 					<div className="glass w-40">
