1bcd776d846576144b0876e0b6d13201934e5606
최영준 <jjmullan24@gmail.com>
2025-12-04 17:31:18 +0900

    [requestLocationData] style: 현재 위치 마커 UI 변경
    ISSUE #22

--- src/features/location/fetch/ui/KakaoMapLocation.tsx
+++ src/features/location/fetch/ui/KakaoMapLocation.tsx
@@ -16,9 +16,18 @@ function KakaoMapLocation() {
 				</MapMarker> */}
 				<MarkerClusterer averageCenter={true} minLevel={10}></MarkerClusterer>
 				<CustomOverlayMap position={{ lat: location!.lat, lng: location!.lng }}>
-					<div className="flex flex-col items-center gap-y-1.5">
-						<TriangleIcon className="rotate-180 w-4 h-4 fill-red-700 animate-bounce" />
-						<div className="rounded-full bg-black w-6 h-6 border-4 border-white" />
+					<div className="flex flex-col items-center gap-y-1">
+						<TriangleIcon className="rotate-180 w-4 h-4 fill-[#e35c18] animate-bounce" />
+						<div className="relative w-6 h-6">
+							{/* 항상 보이는 gradient border (가장 뒤) - 크기 증가 */}
+							<div className="absolute inset-[-8px] rounded-full animate-show-border bg-gradient p-1" />
+
+							{/* 펄스 애니메이션 링 (중간) */}
+							<div className="absolute inset-0 rounded-full animate-pulse-ring bg-gradient-marker p-1" />
+
+							{/* 중앙 검은색 원 (가장 앞) */}
+							<div className="absolute inset-0 rounded-full bg-black m-1 z-1" />
+						</div>
 					</div>
 				</CustomOverlayMap>
 			</Map>
