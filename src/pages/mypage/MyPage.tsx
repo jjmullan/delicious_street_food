48400cd59c51a816c46d18bb3210dd7e55b9c808
최영준 <jjmullan24@gmail.com>
2025-12-05 15:15:21 +0900

    [requestLocationData] feat: mypage router 경로 추가
    ISSUE #22

--- src/pages/mypage/MyPage.tsx
+++ src/pages/mypage/MyPage.tsx
@@ -1,9 +1,10 @@
+import { useParams } from 'react-router';
+
 function MyPage() {
-	return (
-		<>
-			<h1>MyPage Component</h1>
-		</>
-	);
+	const param = useParams();
+	const userId = param.userId;
+
+	return <h1>{userId}의 프로필 상세페이지</h1>;
 }
 
 export default MyPage;
