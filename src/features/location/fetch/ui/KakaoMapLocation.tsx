7b2f2e3fcf876c8ad3c949ecc37b64b948dc85b7
최영준 <jjmullan24@gmail.com>
2025-12-05 20:41:33 +0900

    [requestLocationData] feat: 위치 생성 로직 초안 완성
    - 지도 위치에서 클릭하면 위치 데이터 생성 API 호출
    - supabase 테이블 내 데이터 생성 확인
    
    ISSUE #22

--- src/features/location/fetch/ui/KakaoMapLocation.tsx
+++ src/features/location/fetch/ui/KakaoMapLocation.tsx
@@ -1,11 +1,15 @@
 import { useState } from 'react';
 import { CustomOverlayMap, Map, MarkerClusterer } from 'react-kakao-maps-sdk';
+import { toast } from 'sonner';
 import { useLocation } from '@/app/store/locationStore';
+import { useSession } from '@/app/store/sessionStore';
+import useCreateLocation from '@/features/location/create/hooks/useCreateLocation';
 import { initialLocation } from '@/features/location/fetch/libs/location';
 import type { Location } from '@/features/location/fetch/types/types';
 import CurrentLocation from '@/features/location/fetch/ui/CurrentLocation';
 import SearchLocationBar from '@/features/location/fetch/ui/SearchLocationBar';
 import ProductList from '@/features/product/item/ui/ProductList';
+import useFecthUserData from '@/features/user/fetch/hooks/useFecthUserData';
 
 function KakaoMapLocation() {
 	// LocalStorage 에서 현재 Location 데이터를 가져오기
@@ -17,29 +21,61 @@ function KakaoMapLocation() {
 		lng: location?.lng ?? initialLocation.lng,
 	});
 
+	// 위치 생성 API 호출
+	const { mutate: createLocation, isPending: isCreateLocationPending } = useCreateLocation({
+		onSuccess: () => {
+			toast.info('위치 등록이 성공했습니다.', { position: 'top-center' });
+		},
+		onError: (error) => {
+			toast.error('위치 등록이 실패했습니다.', { position: 'top-center' });
+		},
+	});
+
+	// 세션 데이터 추출
+	const session = useSession();
+	const user_id = session!.user.id;
+	const { error } = useFecthUserData(session?.user.id);
+
+	if (error) {
+		toast.error('유저 데이터 패칭에 실패했습니다.', { position: 'top-center' });
+	}
+
 	return (
 		<div className="relative">
 			<Map
 				center={location ?? initialLocation}
 				level={3}
 				className="min-h-screen"
-				/* onClick={(_, mouseEvent) => {
+				onClick={(_, mouseEvent) => {
 					const latLng = mouseEvent.latLng;
 					const lat = latLng.getLat();
 					const lng = latLng.getLng();
+					console.log('위도:', lat);
+					console.log('경도:', lng);
 
+					// 이전에 클릭한 위치와 현재 클릭한 위치의 값을 절대값으로 치환
 					const latDistance = Math.abs(clickedLocation.lat - lat) * 10_000;
 					const lngDistance = Math.abs(clickedLocation.lng - lng) * 10_000;
 
+					setClickedLocation({
+						lat,
+						lng,
+					});
+
+					// 인접한 위치에 있는 경우, 등록을 취소
 					if (latDistance > 1 || lngDistance > 1) {
-						setClickedLocation({
-							lat,
-							lng,
+						createLocation({
+							user_id,
+							latitude: String(lat),
+							longitude: String(lng),
 						});
 					} else {
 						return;
 					}
-				}} */
+				}}
+				isPanto={true}
+				disableDoubleClick={isCreateLocationPending}
+				disableDoubleClickZoom={isCreateLocationPending}
 			>
 				{/* <MapMarker position={{ lat: location!.lat, lng: location!.lng }}>
 					<div style={{ color: '#000' }}>현재 위치</div>
