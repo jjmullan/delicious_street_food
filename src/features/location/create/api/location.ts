6ea66af5ef8ebfdd578852415cd1ee21684a751d
최영준 <jjmullan24@gmail.com>
2025-12-05 19:59:16 +0900

    [requestLocationData] feat: 위치 데이터 생성 시 컬럼 추가
    - user_id 항목 추가(옵셔널)
    
    ISSUE #22

--- src/features/location/create/api/location.ts
+++ src/features/location/create/api/location.ts
@@ -4,10 +4,11 @@ import type { Location } from '@/shared/types/types';
 /**
  * 위치 정보 데이터 추가 요청 API
  */
-export async function createLocation({ latitude, longitude, location_address, location_name }: Location) {
+export async function createLocation({ user_id, latitude, longitude, location_address, location_name }: Location) {
 	const { data, error } = await supabase
 		.from('location')
 		.insert({
+			user_id,
 			latitude,
 			longitude,
 			location_address,
