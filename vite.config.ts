b2069766e9c68736fdbcc9e8a7e9a85fe9c8740b
최영준 <jjmullan24@gmail.com>
2025-12-06 16:49:57 +0900

    chore: 삭제된 라이브러리 import 삭제

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,11 @@ import path from 'node:path';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
 	// TailwindCSS 플러그인을 React 플러그인과 함께 사용
-	plugins: [react(), tailwindcss(), svgr()],
+	plugins: [react(), tailwindcss()],
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src'),
