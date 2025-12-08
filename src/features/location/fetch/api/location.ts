d9ae7f994ed27e1e10888095e0d0caaa02255cd9
최영준 <jjmullan24@gmail.com>
2025-12-08 17:19:12 +0900

    [createLocationUI] comment: 위치 정보 패칭 기능 주석 추가
    ISSUE #29

--- src/features/location/fetch/api/location.ts
+++ src/features/location/fetch/api/location.ts
@@ -1,6 +1,9 @@
 import supabase from '@/shared/api/supabase/supabase';
 import type { Location } from '@/shared/types/types';
 
+/**
+ * 전체 위치 목록 패칭을 요청하는 API
+ */
 export async function fetchLocation() {
 	const request = supabase.from('location').select('*');
 	const { data, error } = await request;
