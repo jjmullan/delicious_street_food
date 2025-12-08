b34fff0cd2628b01fe74fcebdcefa3298634458b
최영준 <jjmullan24@gmail.com>
2025-12-08 17:37:40 +0900

    [createLocationUI] fix: 전체 위치 목록 패칭 컴포넌트 명칭 수정
    ISSUE #29

--- src/features/location/fetch/api/location.ts
+++ src/features/location/fetch/api/location.ts
@@ -4,7 +4,7 @@ import type { Location } from '@/shared/types/types';
 /**
  * 전체 위치 목록 패칭을 요청하는 API
  */
-export async function fetchLocation() {
+export async function fetchLocations() {
 	const request = supabase.from('location').select('*');
 	const { data, error } = await request;
 
--- src/features/location/fetch/hooks/useFetchLocation.tsx
+++ src/features/location/fetch/hooks/useFetchLocation.tsx
@@ -1,11 +1,11 @@
 import { useQuery } from '@tanstack/react-query';
-import { fetchLocation } from '@/features/location/fetch/api/location';
+import { fetchLocations } from '@/features/location/fetch/api/location';
 import { QUERY_KEYS } from '@/shared/lib/query';
 
 function useFetchLocation() {
 	return useQuery({
 		queryKey: QUERY_KEYS.location.all,
-		queryFn: () => fetchLocation(),
+		queryFn: () => fetchLocations(),
 	});
 }
 
