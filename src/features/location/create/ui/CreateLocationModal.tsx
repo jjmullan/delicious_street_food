e008d469998c4b0db8bdb4ae58a57f42fde4ad98
최영준 <jjmullan24@gmail.com>
2025-12-08 15:17:11 +0900

    [createLocationUI] style: CreateModal 컴포넌트 스타일 수정
    ISSUE #29

--- src/features/location/create/ui/CreateLocationModal.tsx
+++ src/features/location/create/ui/CreateLocationModal.tsx
@@ -22,12 +22,12 @@ function CreateLocationModal() {
 	const store = useCreateLocationModal();
 	if (!store.isOpen) return null;
 
-	const handleCancelClick = () => {
-		if (store.onNegative) store.onNegative();
-		store.actions.close();
-	};
 	const handleActionClick = () => {
 		if (store.onPositive) store.onPositive();
+		store.actions.close();
+	};
+	const handleCancelClick = () => {
+		if (store.onNegative) store.onNegative();
 		store.actions.close();
 	};
 
@@ -36,16 +36,17 @@ function CreateLocationModal() {
 			<AlertDialogContent>
 				<AlertDialogHeader>
 					<AlertDialogTitle>새로운 포장마차를 발견했어요!</AlertDialogTitle>
-					<AlertDialogDescription className="flex flex-col gap-y-2 items-center">
+					<AlertDialogDescription className="sr-only">지도에 신규 포장마차를 등록해주세요</AlertDialogDescription>
+					<div className="text-sm flex flex-col gap-y-2 items-center">
 						<img src={author} alt="author" width={64} />
-						<div className="text-muted-foreground flex justify-center items-center gap-x-1">
+						<p className="text-muted-foreground flex justify-center items-center gap-x-1">
 							발견한 사람: <span className="text-brown-main font-medium">{nickname}</span>
-						</div>
-					</AlertDialogDescription>
+						</p>
+					</div>
 				</AlertDialogHeader>
 				<AlertDialogFooter>
 					<AlertDialogCancel onClick={handleCancelClick}>취소</AlertDialogCancel>
-					<AlertDialogAction onClick={handleActionClick}>지도에 지금 바로 추가하기</AlertDialogAction>
+					<AlertDialogAction onClick={handleActionClick}>지도에 신규 포장마차 추가하기</AlertDialogAction>
 				</AlertDialogFooter>
 			</AlertDialogContent>
 		</AlertDialog>
