b4d1812f2d60efe348eab642335b2861688b6eb6
최영준 <jjmullan24@gmail.com>
2025-12-09 18:13:18 +0900

    [createReview] feat: 리뷰 생성 페이지 초안
    ISSUE #33

--- src/pages/review/ReviewCreatePage.tsx
+++ src/pages/review/ReviewCreatePage.tsx
@@ -1,7 +1,16 @@
+import { useParams } from 'react-router';
+import NonMapHeader from '@/widgets/header/NonMapHeader';
+
 function ReviewCreatePage() {
+	const param = useParams();
+	const location_id = param.locationId;
+	const user_id = param.userId;
+
 	return (
 		<>
-			<h1>ReviewCreatePage Component</h1>
+			<NonMapHeader mode={'extra'} />
+			<p>{location_id}</p>
+			<p>{user_id}</p>
 		</>
 	);
 }
