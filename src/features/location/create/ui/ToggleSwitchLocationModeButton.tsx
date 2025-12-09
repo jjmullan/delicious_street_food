4ea551c78eb4d7f3e6484ba494ba08700552b615
최영준 <jjmullan24@gmail.com>
2025-12-09 17:18:20 +0900

    [extraCreateLocation] feat: 생성 모드 안내 UI 추가
    ISSUE #31

--- src/features/location/create/ui/ToggleSwitchLocationModeButton.tsx
+++ src/features/location/create/ui/ToggleSwitchLocationModeButton.tsx
@@ -10,15 +10,22 @@ function ToggleSwitchLocationModeButton() {
 	};
 
 	return (
-		<button type="button" className={`w-12 h-15 flex justify-center items-center`} onClick={toggleIsUpdateMode}>
-			{/* 위치 생성 모드 상태에 따라 아이콘 변경 */}
-			<Activity mode={isUpdateMode ? 'visible' : 'hidden'}>
-				<Map width={24} color={'#212121'} />
-			</Activity>
+		<div className="relative">
+			<button type="button" className={`w-12 h-15 flex justify-center items-center`} onClick={toggleIsUpdateMode}>
+				{/* 위치 생성 모드 상태에 따라 아이콘 변경 */}
+				<Activity mode={isUpdateMode ? 'visible' : 'hidden'}>
+					<Map width={24} color={'#212121'} />
+				</Activity>
+				<Activity mode={isUpdateMode ? 'hidden' : 'visible'}>
+					<MapPlusIcon width={24} color={'#212121'} />
+				</Activity>
+			</button>
 			<Activity mode={isUpdateMode ? 'hidden' : 'visible'}>
-				<MapPlusIcon width={24} color={'#212121'} />
+				<div className="absolute font-semibold top-[-10px] left-[-36px] bg-brown-sub text-white rounded-full px-2 py-1 w-32 text-[10px] animate-bounce flex items-center justify-center">
+					지도에 포장마차가 없다면?
+				</div>
 			</Activity>
-		</button>
+		</div>
 	);
 }
 
