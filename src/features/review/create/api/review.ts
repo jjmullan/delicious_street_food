c23a89739beb93417e1bc2f7dabb67e280619e47
최영준 <jjmullan24@gmail.com>
2025-12-09 17:45:44 +0900

    [createReview] feat: 리뷰 생성 API 초기 코드 작성
    ISSUE #33

---
+++ src/features/review/create/api/review.ts
@@ -0,0 +1,38 @@
+import supabase from '@/shared/api/supabase/supabase';
+import type { API_Review } from '@/shared/types/types';
+
+/**
+ * 특정 위치에 대해 작성한 상세 리뷰 내용을 생성하는 API
+ */
+export async function createReview({
+	user_id,
+	location_id,
+	product_id,
+	review_title,
+	review_text,
+	is_recommended,
+	order_quantity,
+	order_price,
+	visit_date,
+	visit_time,
+}: API_Review) {
+	const { data, error } = await supabase
+		.from('review')
+		.insert({
+			user_id,
+			location_id,
+			product_id,
+			review_title,
+			review_text,
+			is_recommended,
+			order_quantity,
+			order_price,
+			visit_date,
+			visit_time,
+		})
+		.select()
+		.single();
+
+	if (error) throw error;
+	return data;
+}
