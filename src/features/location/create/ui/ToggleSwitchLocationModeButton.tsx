3e70552a128507b23e1d46ebd25b781c56151b5f
최영준 <jjmullan24@gmail.com>
2025-12-09 17:06:57 +0900

    [extraCreateLocation] style: 위치 생성 모드에 따라 아이콘 변경
    ISSUE #31

--- src/features/location/create/ui/ToggleSwitchLocationModeButton.tsx
+++ src/features/location/create/ui/ToggleSwitchLocationModeButton.tsx
@@ -1,4 +1,5 @@
-import { MapPlusIcon } from 'lucide-react';
+import { Map, MapPlusIcon } from 'lucide-react';
+import { Activity } from 'react';
 import { useIsCreateMode, useSetIsCreateMode } from '@/app/store/createLocationStore';
 
 function ToggleSwitchLocationModeButton() {
@@ -10,7 +11,13 @@ function ToggleSwitchLocationModeButton() {
 
 	return (
 		<button type="button" className={`w-12 h-15 flex justify-center items-center`} onClick={toggleIsUpdateMode}>
-			<MapPlusIcon width={24} color={isUpdateMode ? '#d4944a' : '#212121'} />
+			{/* 위치 생성 모드 상태에 따라 아이콘 변경 */}
+			<Activity mode={isUpdateMode ? 'visible' : 'hidden'}>
+				<Map width={24} color={'#212121'} />
+			</Activity>
+			<Activity mode={isUpdateMode ? 'hidden' : 'visible'}>
+				<MapPlusIcon width={24} color={'#212121'} />
+			</Activity>
 		</button>
 	);
 }
