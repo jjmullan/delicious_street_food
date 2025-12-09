b76117dd8422b18ecd9386223d66002abe79e928
최영준 <jjmullan24@gmail.com>
2025-12-09 13:45:01 +0900

    [extraCreateLocation] style: 검색 바 X 버튼 스타일 수정
    ISSUE #31

--- src/features/location/fetch/ui/SearchLocationBar.tsx
+++ src/features/location/fetch/ui/SearchLocationBar.tsx
@@ -6,7 +6,7 @@ function SearchLocationBar() {
 	const [searchText, setSeartText] = useState('');
 
 	return (
-		<div className="relative">
+		<div className="relative w-[calc(100vw-32px)] max-w-[calc(768px-32px)] min-w-[calc(320px-32px)]">
 			<label htmlFor="searchText" className="sr-only">
 				검색어 입력창
 			</label>
@@ -16,9 +16,9 @@ function SearchLocationBar() {
 				value={searchText}
 				onChange={(e) => setSeartText(e.target.value)}
 				placeholder="검색어를 입력하세요"
-				className="flex justify-between items-center h-12 rounded-full text-md glass border-white/50 shadow-sm pl-4 pr-10 w-[calc(100vw-32px)] max-w-[calc(768px-32px)] min-w-[calc(320px-32px)]"
+				className="flex justify-between items-center h-12 rounded-full text-md glass border-white/50 shadow-sm pl-4 pr-10"
 			/>
-			<button type="button" className="absolute top-4 right-8" onClick={() => setSeartText('')}>
+			<button type="button" className="absolute top-4 right-4" onClick={() => setSeartText('')}>
 				<XCircleIcon className="w-4 h-4" />
 			</button>
 		</div>
