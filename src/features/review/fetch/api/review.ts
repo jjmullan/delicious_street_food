8dea3fd55d75397fc96941ab212c62677e7289c8
최영준 <jjmullan24@gmail.com>
2025-12-10 21:36:01 +0900

    [createReview] feat: Review 패칭 기능 완성
    - UI 작업 필요
    
    ISSUE #33

--- src/features/review/fetch/api/review.ts
+++ src/features/review/fetch/api/review.ts
@@ -1,10 +1,11 @@
 import supabase from '@/shared/api/supabase/supabase';
+import type { Review } from '@/shared/types/types';
 
 /**
  * 특정 위치에 작성된 모든 리뷰 정보를 패칭하는 API
  */
-export async function fetchReviewsByLocation(location_id: string) {
-	const { data, error } = await supabase.from('location').select('*').eq('location_id', location_id);
+export async function fetchReviewsByLocation(location_id: string): Promise<Review[]> {
+	const { data, error } = await supabase.from('review').select('*').eq('location_id', location_id);
 
 	if (error) throw error;
 	return data;
--- src/pages/location/LocationPage.tsx
+++ src/pages/location/LocationPage.tsx
@@ -1,12 +1,24 @@
 import { Link, useParams } from 'react-router';
+import useFetchReviewsByLocation from '@/features/review/fetch/hook/useFetchReviewsByLocation';
 
 function LocationPage() {
 	const param = useParams();
 	const locationId = param.locationId;
+	const { data: fetchReviews, isPending: isFetchReviewsPending } = useFetchReviewsByLocation(locationId!);
+
+	// Pending 통합 상태 관리
+	const isPending = isFetchReviewsPending;
 
 	return (
 		<>
-			<h1>{locationId}의 장소 상세페이지</h1>
+			<div>
+				{fetchReviews?.map((review) => (
+					<div key={review.review_id}>
+						<div>{review.review_title}</div>
+						<div>{review.review_text}</div>
+					</div>
+				))}
+			</div>
 			<Link to={`/location/${locationId}/review/all`}>리뷰</Link>
 		</>
 	);
