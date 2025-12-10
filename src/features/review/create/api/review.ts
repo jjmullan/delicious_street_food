f77e1797dddc1a87ee041904a8f4c794eb273235
최영준 <jjmullan24@gmail.com>
2025-12-10 21:30:24 +0900

    [createReview] feat: 후기 등록 이미지 API 요청 기능 추가
    ISSUE #33

--- src/features/review/create/api/review.ts
+++ src/features/review/create/api/review.ts
@@ -1,5 +1,5 @@
 import supabase from '@/shared/api/supabase/supabase';
-import type { API_Review, API_ReviewProduct } from '@/shared/types/types';
+import type { API_Review, API_ReviewImage, API_ReviewProduct } from '@/shared/types/types';
 
 /**
  * 특정 위치에 대해 작성한 상세 리뷰 내용을 생성하는 API
@@ -47,6 +47,23 @@ export async function createReviewProduct({
 			is_recommend,
 			order_price,
 			order_quantity,
+		})
+		.select()
+		.single();
+
+	if (error) throw error;
+	return data;
+}
+
+/**
+ * 특정 위치에 대해 업로드하려는 이미지 목록 데이터를 추가하는 API
+ */
+export async function createReviewImages({ review_id, review_image_url }: API_ReviewImage) {
+	const { data, error } = await supabase
+		.from('review_image')
+		.insert({
+			review_id,
+			review_image_url,
 		})
 		.select()
 		.single();
