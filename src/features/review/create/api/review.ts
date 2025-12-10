b72a0cb8437b1bfab4bcdb98e9fde10b5bc2b623
최영준 <jjmullan24@gmail.com>
2025-12-10 21:29:20 +0900

    [createReview] feat: 후기 등록 상품 생성 API 요청 기능 추가
    ISSUE #33

--- src/features/review/create/api/review.ts
+++ src/features/review/create/api/review.ts
@@ -1,5 +1,5 @@
 import supabase from '@/shared/api/supabase/supabase';
-import type { API_Review } from '@/shared/types/types';
+import type { API_Review, API_ReviewProduct } from '@/shared/types/types';
 
 /**
  * 특정 위치에 대해 작성한 상세 리뷰 내용을 생성하는 API
@@ -21,6 +21,32 @@ export async function createReview({
 			review_text,
 			is_recommended,
 			visit_datetime,
+		})
+		.select()
+		.single();
+
+	if (error) throw error;
+	return data;
+}
+
+/**
+ * 상세 리뷰 내용 중 구매한 상품에 대한 개별 목록을 생성하는 API
+ */
+export async function createReviewProduct({
+	review_id,
+	product_id,
+	is_recommend,
+	order_price,
+	order_quantity,
+}: API_ReviewProduct) {
+	const { data, error } = await supabase
+		.from('review_product')
+		.insert({
+			review_id,
+			product_id,
+			is_recommend,
+			order_price,
+			order_quantity,
 		})
 		.select()
 		.single();
