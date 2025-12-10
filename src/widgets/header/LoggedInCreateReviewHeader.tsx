30971f97fee2fed2e612123f409074a4ae4eacd7
최영준 <jjmullan24@gmail.com>
2025-12-10 17:18:38 +0900

    [createReview] feat: 로그인 전용 상세 페이지 레이아웃 스타일 수정
    - 리뷰 생성 페이지 전용 헤더 추가
    
    ISSUE #33

---
+++ src/widgets/header/LoggedInCreateReviewHeader.tsx
@@ -0,0 +1,53 @@
+import { RefreshCcwIcon, XCircleIcon } from 'lucide-react';
+import { useNavigate } from 'react-router';
+import { toast } from 'sonner';
+import { useOpenConfirmModal } from '@/app/store/confirmModalStore';
+
+function LoggedInCreateReviewHeader() {
+	const navigate = useNavigate();
+	const openConfirmModal = useOpenConfirmModal();
+	const handleClickCancelButton = () => {
+		openConfirmModal({
+			title: '리뷰 작성을 취소하시겠습니까?',
+			description: '작성된 모든 정보가 삭제됩니다.',
+			onPositive: () => {
+				toast.info('리뷰 작성이 취소되었습니다.', { position: 'top-center' });
+				navigate('/', { replace: true });
+			},
+		});
+		return;
+	};
+	const handleClickResetButton = () => {
+		openConfirmModal({
+			title: '작성한 내용을 초기화하시겠습니까?',
+			description: '작성된 모든 정보가 삭제됩니다.',
+			onPositive: () => {
+				toast.info('작성한 내용이 초기화되었습니다.', { position: 'top-center' });
+				window.location.reload();
+			},
+		});
+		return;
+	};
+
+	return (
+		<header className="fixed flex justify-between items-center text-sm w-full bg-[#fff]">
+			<button
+				type="button"
+				className="cursor-pointer px-4 h-12 flex justify-center items-center"
+				onClick={handleClickCancelButton}
+			>
+				<XCircleIcon width={16} />
+			</button>
+			<h2 className="text-lg font-semibold">리뷰 작성</h2>
+			<button
+				type="button"
+				className="cursor-pointer px-4 h-12 flex justify-center items-center"
+				onClick={handleClickResetButton}
+			>
+				<RefreshCcwIcon width={16} />
+			</button>
+		</header>
+	);
+}
+
+export default LoggedInCreateReviewHeader;
--- src/widgets/layout/LoggedInDetailLayout.tsx
+++ src/widgets/layout/LoggedInDetailLayout.tsx
@@ -1,18 +1,18 @@
 import { Navigate, Outlet } from 'react-router';
 import { useSession } from '@/app/store/sessionStore';
-import LoggedInDetailHeader from '@/widgets/header/LoggedInDetailHeader';
+import LoggedInCreateReviewHeader from '@/widgets/header/LoggedInCreateReviewHeader';
 
 /**
  * 최초 서비스 접근 시, 로그인 페이지로 라우팅
  */
 function LoggedInDetailLayout() {
 	const session = useSession();
-
 	if (!session) return <Navigate to={'/login'} replace={true} />;
+
 	return (
 		<div className="min-h-svh">
-			<LoggedInDetailHeader />
-			<main className="p-3">
+			<LoggedInCreateReviewHeader />
+			<main className="p-3 mt-12 mb-16">
 				<Outlet />
 			</main>
 		</div>
