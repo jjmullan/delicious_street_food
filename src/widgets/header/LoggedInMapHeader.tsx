2a0577940ebad97e9c1fd31f5f0fb46e9a63a464
최영준 <jjmullan24@gmail.com>
2025-12-12 14:42:13 +0900

    [reviewImage] style: 헤더 반응형 스타일 조정
    - 768px 이상 넘어가는 현상 수정
    
    ISSUE #35

--- src/widgets/header/LoggedInMapHeader.tsx
+++ src/widgets/header/LoggedInMapHeader.tsx
@@ -4,7 +4,7 @@ import HomeButton from '@/shared/ui/button/HomeButton';
 
 function LoggedInMapHeader() {
 	return (
-		<header className="flex justify-around items-center fixed text-lg z-99 glass rounded-t-2xl bottom-0 w-full h-fit pb-3">
+		<header className="flex justify-around items-center fixed text-lg z-99 glass rounded-t-2xl bottom-0 full-width h-fit pb-3">
 			{/* <ItemListButton /> */}
 			<ToggleSwitchLocationModeButton />
 			<HomeButton />
