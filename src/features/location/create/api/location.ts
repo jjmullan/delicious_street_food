840c10ab86682db01ebf913f71d394dedd3a4c1e
최영준 <jjmullan24@gmail.com>
2025-12-14 16:46:23 +0900

    [locationDetail] feat: 신규 포장마차 이름 데이터 전송 로직 추가
    - 이름 유무 검증
    - 이름이 있는 경우 입력창 제공
    - 작성된 이름이 있다면 해당 이름으로, 없다면 '포장마차'로 API 전송
    
    ISSUE #37

--- src/features/location/create/api/location.ts
+++ src/features/location/create/api/location.ts
@@ -1,16 +1,22 @@
 import supabase from '@/shared/api/supabase/supabase';
-import type { API_Location } from '@/shared/types/types';
+import type { API_Location, Location } from '@/shared/types/types';
 
 /**
  * 위치 정보 데이터 추가 요청 API
  */
-export async function createLocation({ user_id, latitude, longitude }: API_Location) {
+export async function createLocation({
+	user_id,
+	latitude,
+	longitude,
+	location_name,
+}: API_Location & Partial<Pick<Location, 'location_name'>>) {
 	const { data, error } = await supabase
 		.from('location')
 		.insert({
 			user_id,
 			latitude,
 			longitude,
+			location_name,
 		})
 		.select()
 		.single();
