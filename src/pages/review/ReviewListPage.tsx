a81a0e6d37f45a1d4a7d218d3233b00c4d3db0ee
최영준 <jjmullan24@gmail.com>
2025-12-15 00:15:08 +0900

    [locationDetail] feat: 리뷰 목록 페이지 UI 완성
    - (개선) 무한스크롤 기능 구현 필요
    
    ISSUE #37

--- src/pages/review/ReviewListPage.tsx
+++ src/pages/review/ReviewListPage.tsx
@@ -6,7 +6,6 @@ import LocationNavigation from '@/entities/location/ui/LocationNavigation';
 import useFetchReviewsByLocation from '@/features/review/fetch/hook/useFetchReviewsByLocation';
 import ReviewItem from '@/features/review/fetch/ui/ReviewItem';
 import { Button } from '@/shared/ui/shadcn/button';
-import LoggedInDetailHeader from '@/widgets/header/LoggedInDetailHeader';
 
 function ReviewListPage() {
 	const navigate = useNavigate();
@@ -38,15 +37,22 @@ function ReviewListPage() {
 						<p>리뷰 작성하러 가기</p>
 					</Button>
 				</div>
-				<div className="flex flex-col mt-39 px-3">
+				<div className="flex flex-col mt-39 px-3 pb-3">
 					<Activity mode={isPending ? 'hidden' : 'visible'}>
-						{fetchReviews?.map((review) => (
-							<ReviewItem key={review.review_id} {...review} />
-						))}
+						{/* 리뷰가 없을 때 대체 UI */}
 						<Activity mode={fetchReviews?.length === 0 ? 'visible' : 'hidden'}>
 							<div className="flex flex-col justify-center items-center gap-y-2 min-h-[calc(100svh-160px)]">
 								<InfoIcon width={48} height={48} className="stroke-[1.5]" />
 								<p className="font-medium">작성된 리뷰가 없습니다.</p>
+							</div>
+						</Activity>
+						{/* 리뷰가 있을 때 */}
+						<Activity mode={fetchReviews && fetchReviews.length > 0 ? 'visible' : 'hidden'}>
+							{fetchReviews?.map((review) => (
+								<ReviewItem key={review.review_id} {...review} />
+							))}
+							<div className="flex justify-center items-center p-3 text-sm border border-t-0 bg-muted">
+								마지막 리뷰입니다
 							</div>
 						</Activity>
 					</Activity>
