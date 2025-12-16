99e6a0cdcfb67404b55c0fa17a4ecf9bdb2d2cec
최영준 <jjmullan24@gmail.com>
2025-12-17 01:36:42 +0900

    [mapUI] feat: 지도 위치 클릭 시 랜덤 리뷰 이미지 제공 기능 추가
    ISSUE #39

--- src/features/location/fetch/ui/LocationInfoModal.tsx
+++ src/features/location/fetch/ui/LocationInfoModal.tsx
@@ -1,18 +1,29 @@
 import { PopoverClose } from '@radix-ui/react-popover';
 import foodstall from '@shared/assets/character/foodstall.svg';
-import { BookmarkIcon, CameraIcon, MessageCircleMoreIcon, PenBoxIcon, ThumbsUpIcon } from 'lucide-react';
+import { BookmarkIcon, CameraIcon, MessageCircleMoreIcon, PenBoxIcon } from 'lucide-react';
+import { Activity, useMemo } from 'react';
 import { Link } from 'react-router';
+import useFetchFavorite from '@/features/favorite/fetch/hooks/useFetchFavorite';
+import useFetchReviewImagesByLocation from '@/features/review/fetch/hook/useFetchReviewImagesByLocation';
+import useFetchReviewsByLocation from '@/features/review/fetch/hook/useFetchReviewsByLocation';
+import { getRandomArrayItem } from '@/shared/lib/utils';
 import type { Location, User } from '@/shared/types/types';
 import { Popover, PopoverContent, PopoverTrigger } from '@/shared/ui/shadcn/popover';
 
-function LocationInfoModal({
-	userData,
-	location_id,
-	total_recommend_count,
-	total_review_count,
-	total_favorite_count,
-}: Partial<Location> & { userData: Partial<User> }) {
-	const user_id = userData?.user_id;
+function LocationInfoModal({ location_id }: Partial<Location> & { userData: Partial<User> }) {
+	const { data: fetchReviewImages, isPending: isFetchReviewImagesPending } = useFetchReviewImagesByLocation(
+		location_id!
+	);
+	const { data: fetchReviews, isPending: isFetchReviewsPending } = useFetchReviewsByLocation(location_id!);
+	const { data: fetchFavorites, isPending: isFetchFavoritesPending } = useFetchFavorite(location_id!);
+
+	// 임의의 리뷰 이미지 선택
+	const randomReviewImage = useMemo(() => {
+		return getRandomArrayItem(fetchReviewImages || []);
+	}, [fetchReviewImages]);
+
+	// Pending 통합 상태 관리
+	const isPending = isFetchReviewImagesPending || isFetchReviewsPending || isFetchFavoritesPending;
 
 	return (
 		<Popover>
@@ -32,34 +43,38 @@ function LocationInfoModal({
 					<div className="flex flex-col justify-center rounded-md shadow-md w-fit">
 						<Link
 							to={`/location/${location_id}/home`}
-							className="flex flex-col justify-center gap-y-2 items-center p-3 rounded-md shadow-md"
+							className="flex flex-col justify-center gap-y-2 items-center p-1 rounded-md shadow-md"
 						>
-							<div className="relative w-24 h-20">
+							<div className="relative w-24 h-24">
 								{/* 대표 이미지 */}
-								<div className="absolute inset-0 bg-gray-100 border-2 border-gray-400 rounded-md flex flex-col text-[10px] items-center justify-center gap-y-1">
-									<CameraIcon />
-									<p>이미지가 없습니다</p>
-								</div>
-								{/* <img src={} className="cursor-pointer rounded-full object-cover" alt="user profile" /> */}
+								<Activity mode={randomReviewImage ? 'visible' : 'hidden'}>
+									<img
+										src={randomReviewImage?.review_image_url}
+										alt="리뷰 이미지"
+										className="absolute inset-0 w-full h-full object-cover rounded-md border-gray-400"
+									/>
+								</Activity>
+								<Activity mode={!randomReviewImage ? 'visible' : 'hidden'}>
+									<div className="absolute inset-0 bg-gray-100 border-2 border-gray-400 rounded-md flex flex-col text-[10px] items-center justify-center gap-y-1">
+										<CameraIcon />
+										<p>이미지가 없습니다</p>
+									</div>
+								</Activity>
 							</div>
-							<div className="flex justify-around items-center w-full text-xs">
+							<div className="flex justify-center items-center gap-x-2 w-full text-sm pb-2">
 								<div className="flex gap-x-1 items-center">
-									<ThumbsUpIcon width={12} height={12} />
-									<p>{total_recommend_count! >= 100 ? 99 : total_recommend_count}</p>
+									<MessageCircleMoreIcon width={16} height={16} />
+									<p>{fetchReviews && fetchReviews.length! >= 100 ? 99 : fetchReviews?.length}</p>
 								</div>
 								<div className="flex gap-x-1 items-center">
-									<MessageCircleMoreIcon width={12} height={12} />
-									<p>{total_review_count! >= 100 ? 99 : total_review_count}</p>
-								</div>
-								<div className="flex gap-x-1 items-center">
-									<BookmarkIcon width={12} height={12} />
-									<p>{total_favorite_count! >= 100 ? 99 : total_favorite_count}</p>
+									<BookmarkIcon width={16} height={16} />
+									<p>{fetchFavorites && fetchFavorites.length >= 100 ? 99 : fetchFavorites?.length}</p>
 								</div>
 							</div>
 						</Link>
 						<Link
 							to={`/location/${location_id}/review/new`}
-							className="flex gap-x-1.5 justify-center items-center text-xs p-3 border-t"
+							className="flex gap-x-1.5 justify-center items-center text-sm p-3 border-t"
 						>
 							<PenBoxIcon width={12} height={12} />
 							<p>리뷰 작성하기</p>
