66eb19c288bce313b67f8225dc7a067960055b02
최영준 <jjmullan24@gmail.com>
2025-12-22 10:50:56 +0900

    feat: 상품 리스트 추가건 반영
    - 땅콩빵

--- src/features/product/item/libs/item.ts
+++ src/features/product/item/libs/item.ts
@@ -29,12 +29,17 @@ export const items: Item[] = [
 		name_ko: '호두과자',
 		name_en: 'walnutcake',
 	},
+	{
+		name_ko: '땅콩방',
+		name_en: 'peanutbread',
+	},
 ];
 
 import eggbun from '@shared/assets/character/eggbun.svg';
 import fishshapedbun from '@shared/assets/character/fishshapedbun.svg';
 import hoppang from '@shared/assets/character/hoppang.svg';
 import hotteok from '@shared/assets/character/hotteok.svg';
+import peanutbread from '@shared/assets/character/peanutbread.svg';
 import roastedchestnuts from '@shared/assets/character/roastedchestnuts.svg';
 import roastedsweetpotato from '@shared/assets/character/roastedsweetpotato.svg';
 import walnutcake from '@shared/assets/character/walnutcake.svg';
@@ -48,4 +53,5 @@ export const characterImages: Record<string, string> = {
 	roastedchestnuts,
 	roastedsweetpotato,
 	walnutcake,
+	peanutbread,
 };
