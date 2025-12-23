e6253196bec6bc445d8a1990fa3ac707464640ff
최영준 <jjmullan24@gmail.com>
2025-12-23 15:05:00 +0900

    [mypageEtc] feat: 리뷰 수정 기능 비활성화
    ISSUE #45

--- src/shared/ui/button/EditDeleteButton.tsx
+++ src/shared/ui/button/EditDeleteButton.tsx
@@ -1,5 +1,5 @@
 import { PopoverClose } from '@radix-ui/react-popover';
-import { EditIcon, MoreVerticalIcon, Trash2Icon } from 'lucide-react';
+import { MoreVerticalIcon, Trash2Icon } from 'lucide-react';
 import { Popover, PopoverContent, PopoverTrigger } from '@/shared/ui/shadcn/popover';
 
 function EditDeleteButton({ onDelete }: { onDelete(): void }) {
@@ -9,14 +9,14 @@ function EditDeleteButton({ onDelete }: { onDelete(): void }) {
 				<MoreVerticalIcon width={16} height={16} />
 			</PopoverTrigger>
 			<PopoverContent className="flex w-fit flex-col justify-center items-center text-sm p-0">
-				<PopoverClose asChild>
+				{/* <PopoverClose asChild>
 					<button type="button" className="flex justify-center items-center gap-x-1 p-3 pb-1.5">
 						<EditIcon width={12} height={12} />
 						<p className="text-xs">수정</p>
 					</button>
-				</PopoverClose>
+				</PopoverClose> */}
 				<PopoverClose asChild>
-					<button type="button" className="flex justify-center items-center gap-x-1 p-3 pt-1.5" onClick={onDelete}>
+					<button type="button" className="flex justify-center items-center gap-x-1 p-3" onClick={onDelete}>
 						<Trash2Icon width={12} height={12} />
 						<p className="text-xs">삭제</p>
 					</button>
