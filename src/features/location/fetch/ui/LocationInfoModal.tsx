3ab778494d68c2447e8a95301d553705d9fd8a01
최영준 <jjmullan24@gmail.com>
2025-12-23 17:53:30 +0900

    [mypageEtc] feat: 매장명, 매장 위치 UI 추가
    ISSUE #45

--- src/features/location/fetch/ui/LocationInfoModal.tsx
+++ src/features/location/fetch/ui/LocationInfoModal.tsx
@@ -7,13 +7,18 @@ import foodstall from '@shared/assets/character/foodstall.svg';
 import { getRandomArrayItem } from '@shared/lib/utils';
 import type { Location, User } from '@shared/types/types';
 import { Popover, PopoverContent, PopoverTrigger } from '@shared/ui/shadcn/popover';
-import { CameraIcon, HeartIcon, MessageCircleMoreIcon, PenBoxIcon } from 'lucide-react';
+import { CameraIcon, HeartIcon, MessageCircleMoreIcon } from 'lucide-react';
 import { Activity, useMemo } from 'react';
 import { Link } from 'react-router';
+import { useLocation } from '@/app/store/locationStore';
+import { calculateDistanceFromLocation } from '@/features/location/create/utils/validateLocationDistance';
 
 function LocationInfoModal({
 	userData,
 	location_id,
+	latitude,
+	longitude,
+	location_name,
 	product_name_en,
 }: Partial<Location> & { userData: Partial<User> } & { product_name_en?: string }) {
 	// 데이터 패칭
@@ -28,6 +33,11 @@ function LocationInfoModal({
 		if (!fetchFavorites || !userData?.user_id) return false;
 		return fetchFavorites.some((favorite) => favorite.user_id === userData.user_id);
 	}, [fetchFavorites, userData?.user_id]);
+
+	// 현재 위치와 장소와의 위치를 계산
+	const location = useLocation();
+	const distance = calculateDistanceFromLocation({ lat: Number(latitude), lng: Number(longitude) }, location);
+	const fotmattedDistance = distance > 1000 ? `${(distance / 1000).toFixed(1)}km` : `${distance.toFixed()}m`;
 
 	// 임의의 후기 이미지 선택
 	const randomReviewImage = useMemo(() => {
@@ -67,8 +77,8 @@ function LocationInfoModal({
 							to={`/location/${location_id}/home`}
 							className="flex flex-col justify-center gap-y-2 items-center p-2 rounded-md shadow-md"
 						>
-							<div className="relative w-24 h-24">
-								{/* 대표 이미지 */}
+							{/* 대표 이미지 */}
+							<div className="relative w-full h-24">
 								<Activity mode={randomReviewImage ? 'visible' : 'hidden'}>
 									<img
 										src={randomReviewImage?.review_image_url}
@@ -83,7 +93,20 @@ function LocationInfoModal({
 									</div>
 								</Activity>
 							</div>
-							<div className="flex justify-center items-center gap-x-2 w-full text-sm">
+							<div className="flex flex-col items-center justify-center text-sm">
+								<Activity mode={location_name ? 'visible' : 'hidden'}>
+									<p className="font-medium">{location_name}</p>
+								</Activity>
+								<Activity mode={location_name ? 'hidden' : 'visible'}>
+									<p className="text-muted-foreground">포장마차</p>
+								</Activity>
+								<div className="flex justify-center items-center gap-x-0.5">
+									{/* <MousePointer2Icon width={14} height={14} strokeWidth={1.8} className="scale-x-[-1]" /> */}
+									<p className="text-xs">현재 위치로부터 {fotmattedDistance}</p>
+								</div>
+							</div>
+							{/* 후기 & 즐겨찾기 */}
+							<div className="flex justify-center items-center gap-x-2 w-full text-sm border-t pt-2">
 								<div className="flex gap-x-1 items-center">
 									<MessageCircleMoreIcon width={16} height={16} />
 									<p>{fetchReviews && fetchReviews.length! >= 100 ? 99 : fetchReviews?.length}</p>
@@ -94,13 +117,13 @@ function LocationInfoModal({
 								</div>
 							</div>
 						</Link>
-						<Link
+						{/* <Link
 							to={`/location/${location_id}/review/new`}
 							className="flex gap-x-1.5 justify-center items-center text-sm p-3 border-t"
 						>
 							<PenBoxIcon width={12} height={12} />
 							<p>후기 작성하기</p>
-						</Link>
+						</Link> */}
 					</div>
 				</PopoverClose>
 			</PopoverContent>
