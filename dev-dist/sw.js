ea706aaeae32a41e86bca3746745068d9c9b13d9
최영준 <jjmullan24@gmail.com>
2026-07-17 18:51:37 +0900

    chore: husky 비활성화

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -67,7 +67,7 @@ if (!self.define) {
     });
   };
 }
-define(['./workbox-08e2db5c'], (function (workbox) { 'use strict';
+define(['./workbox-52f2a342'], (function (workbox) { 'use strict';
 
   self.skipWaiting();
   workbox.clientsClaim();
@@ -82,11 +82,12 @@ define(['./workbox-08e2db5c'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.dt4dhqn38ro"
+    "revision": "0.fnpgqvjphg8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
-    allowlist: [/^\/$/]
+    allowlist: [/^\/$/],
+    denylist: [/^\/api\//]
   }));
   workbox.registerRoute(/^https:\/\/dapi\.kakao\.com\/.*/i, new workbox.NetworkFirst({
     "cacheName": "kakao-map-cache",
